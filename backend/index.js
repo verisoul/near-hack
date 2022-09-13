b0e6783f4d79070c9708a63ba40b2b6db00bce67
rainems <rainems3@berkeley.edu>
2022-09-13 15:39:11 +0100

    Error handling on completed page (#5)
    Co-authored-by: rainems3 <rainems3@gmail.com>

--- backend/index.js
+++ backend/index.js
@@ -1,25 +1,23 @@
 const {api, data, http} = require("@serverless/cloud");
 const cors = require("cors")
-const {authenticate, addUserToDAO} = require("./server/near")
+const {authenticate, addUserToDAO, voteOnProposal, addUserToVerisoulContract} = require("./server/near")
 const {getSession, postSession} = require("./server/api")
+
+const DAO_NAME = "nearcon"
+const DAO_ID = DAO_NAME + ".sputnik-dao.near"
+const PROJECT = "Nearcon_Afterparty_DAO"
+
 
 api.use(cors());
 
-api.get("/callback", async (req, res) => {
-
-    console.log(req)
-    // TODO save the session id and vote to add user to DAO
-    res.send('Ok')
-})
-
 // Catch all for missing API routes
-api.get("/complete", async (req, res) => {
+api.get("/complete", { timeout: 30000 }, async (req, res) => {
     let session = req.query.sessionId
     let project = req.query.project
 
-    let sessionResult = await getSession(session, project)
-    console.log(sessionResult)
-    let {isSessionComplete, externalId, isBlocked, hasBlockedAccounts, numCompletedSessions} = sessionResult
+    const [account, sessionResult] = await Promise.all([authenticate(), getSession(session, project)])
+
+    let {isSessionComplete, externalId, isBlocked, hasBlockedAccounts, numAccounts} = sessionResult
 
     if (!isSessionComplete) {
         res.send({error: "Session is not complete"})
@@ -33,19 +31,27 @@ api.get("/complete", async (req, res) => {
         res.send({error: "User has blocked accounts"})
         return;
     }
-    if (numCompletedSessions >= 2) {
+    if (numAccounts >= 2) {
         res.send({error: "User has already signed up"})
         return;
     }
 
+    const [proposalId, inVerisoulContract] = await Promise.all([
+            addUserToDAO(account, DAO_ID, externalId),
+            addUserToVerisoulContract(account, PROJECT, externalId)
+        ]
+    )
 
-    let account = await authenticate()
-    let smartContractResponse = await addUserToDAO(account, 'nearcon', externalId, "https://webhook.site/e5596df5-fa5b-4432-8e49-978b211647a2")
-    console.log(smartContractResponse)
-    console.log("Proposal added to the DAO");
-    // TODO save the session status to track callback
+    if(!proposalId){
+        res.send({error: "Could not create proposal"})
+        return;
+    }
 
-    res.send({ok: 'ok'})
+    const proposal_url = `https://app.astrodao.com/dao/${DAO_ID}/proposals/${DAO_ID}-${proposalId}`
+    await voteOnProposal(account, DAO_ID, proposalId)
+    console.log("Proposal voted on");
+
+    res.send({ok: 'ok', proposal: proposal_url, inVerisoulContract})
 });
 
 api.get("/session", async (req, res) => {
@@ -54,13 +60,5 @@ api.get("/session", async (req, res) => {
         console.log(project, address);
         let session = await postSession(project, address)
         res.send({session})
-    }
-)
-
-api.get("/findSession", async (req, res) => {
-        let sessionId = req.query.sessionId
-        let project = req.query.project
-        let sessionResult = await getSession(sessionId, project)
-        res.send(sessionResult)
     }
 )
